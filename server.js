5ce175ddc81eb38d39f682558ecb20b4fcfdfaf1
nithish1729 <nithish1729@gmail.com>
2023-08-09 12:29:12 +0530

    FIRST COMMIT

---
+++ server.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const studentsRoutes = require('./src/student/routes')
+const app=express();
+const port= 3000;
+
+app.use(express.json()); //middleware
+
+app.get("/",(req,res) => {
+    res.send("Hello World")
+});
+
+app.use("/api/v1/students",studentsRoutes);
+
+app.listen(port, () => console.log(`app listening on port ${port}`));
